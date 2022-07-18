a29c3bb98edce3db01cf37d775fe0c6c0fc9e256
Abdulrahman-jarkas <abdulrahman.jarkas.business@gmail.com>
2022-07-18 13:40:06 +0300

    lazy load modules

--- src/app/movies/movies.module.ts
+++ src/app/movies/movies.module.ts
@@ -5,6 +5,7 @@ import { MoviesRoutingModule } from './movies-routing.module';
 import { MoviesListComponent } from './movies-list/movies-list.component';
 import { MoviesComponent } from './movies/movies.component';
 import { MovieDetailsComponent } from './movie-details/movie-details.component';
+import { MoviesService } from './service/movies.service';
 
 
 @NgModule({
@@ -16,6 +17,7 @@ import { MovieDetailsComponent } from './movie-details/movie-details.component';
   imports: [
     CommonModule,
     MoviesRoutingModule
-  ]
+  ],
+  providers:[MoviesService]
 })
 export class MoviesModule { }
